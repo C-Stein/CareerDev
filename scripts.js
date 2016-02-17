f87a92d4736ee46384631bb4db62eb1e21cf0e9c
Caitlin Stein <caitlin.teter@gmail.com>
2016-02-17 13:44:10 -0600

    login in process

--- scripts.js
+++ scripts.js
@@ -1,2 +1,21 @@
 console.log("javascript is present!");
+
 OAuth.initialize('Jm26g27oCrprH3FOLnc8GRyi4gs');
+
+OAuth.popup('github')
+  .done(function(result) {
+       result.get('/me')
+    .done(function (response) {
+        //this will display "John Doe" in the console
+        console.log(response.name);
+    })
+    .fail(function (err) {
+        console.log("error #1!", err);
+    });
+})
+.fail(function (err) {
+    //handle error with err
+    console.log("error #2!", err);
+});
+  
+console.log("still running js");
