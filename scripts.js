17c65108ccdeda5b103203a3903db993642098ee
Caitlin Stein <caitlin.teter@gmail.com>
2016-02-26 13:35:46 -0600

    put authorization behind a button

--- scripts.js
+++ scripts.js
@@ -2,29 +2,31 @@ console.log("javascript is present!");
 
 OAuth.initialize('Jm26g27oCrprH3FOLnc8GRyi4gs');
 
-OAuth.popup('github')
-  .done(function(result) {
-       result.get('/me')
-    .done(function (response) {
-        //this will display "John Doe" in the console
-        console.log(response.name);
+document.getElementById('button').addEventListener("click", function() {
+    OAuth.popup('google')
+      .done(function(result) {
+            console.log("it worked");
+           result.get('/me')
+        .done(function (response) {
+            //this will display "John Doe" in the console
+            console.log(response.name);
+        })
+        .fail(function (err) {
+            console.log("error #1!", err);
+        });
     })
     .fail(function (err) {
-        console.log("error #1!", err);
+        //handle error with err
+        console.log("error #2!", err);
     });
-})
-.fail(function (err) {
-    //handle error with err
-    console.log("error #2!", err);
 });
-  
 console.log("still running js");
 
-OAuth.popup('github').done(function(github) {
-    github.me().done(function(me) {
-        console.log('Your name is ' + me.name)
-    })
-}).fail(function (err) {
-    //handle error with err
-    console.log("error the other one!", err);
-});
+// OAuth.popup('github').done(function(github) {
+//     github.me().done(function(me) {
+//         console.log('Your name is ' + me.name)
+//     })
+// }).fail(function (err) {
+//     //handle error with err
+//     console.log("error the other one!", err);
+// });
