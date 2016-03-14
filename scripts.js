25f0c8929adc0c94326bbf46d379c6617c020611
Caitlin Stein <caitlin.teter@gmail.com>
2016-03-14 13:10:25 -0500

    basic authentication behind a button

--- scripts.js
+++ scripts.js
@@ -1,32 +1,14 @@
-console.log("javascript is present!");
-
-OAuth.initialize('Jm26g27oCrprH3FOLnc8GRyi4gs');
+OAuth.initialize('5oulLt6uDhIImPaRvfaq80gzwGo');
 
 document.getElementById('button').addEventListener("click", function() {
-    OAuth.popup('github')
-      .done(function(result) {
-            console.log("it worked");
-           result.get('/me')
-        .done(function (response) {
-            //this will display "John Doe" in the console
-            console.log(response.name);
-        })
-        .fail(function (err) {
-            console.log("error #1!", err);
-        });
+  OAuth.popup('github')
+    .done(function(result) {
+      console.log("it worked");
+      console.log("result", result);
     })
     .fail(function (err) {
-        //handle error with err
         console.log("error #2!", err);
     });
+    
 });
-console.log("still running js");
 
-// OAuth.popup('github').done(function(github) {
-//     github.me().done(function(me) {
-//         console.log('Your name is ' + me.name)
-//     })
-// }).fail(function (err) {
-//     //handle error with err
-//     console.log("error the other one!", err);
-// });
